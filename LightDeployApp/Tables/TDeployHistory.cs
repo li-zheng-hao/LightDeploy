c589630c1cd681fb35d196d61dc647737e9ed820
LiZhengHao <1263212577@qq.com>
2023-11-09 14:33:06 +0800

    支持回滚

---
+++ LightDeployApp/Tables/TDeployHistory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LightDeployApp.Tables;
+
+/// <summary>
+/// 部署历史
+/// </summary>
+public class TDeployHistory:ViewModelBase
+{
+    [ColumnName("部署时间")]
+    public DateTime CreateTime { get; set; }
+
+    [ColumnName("部署文件夹")]
+    public string DeployFilesDir { get; set; }
+
+    [ColumnName("备注")]
+    public string Remark { get; set; }
+    
+    [ColumnName("服务名称")]
+    public string ServiceName { get; set; }
+}
